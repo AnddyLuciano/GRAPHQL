1759a53b6f55d3672f8c84033430e4dd568d3951
Anddy Luciano <luciano.ratsimbazafindramanana@gmail.com>
2023-10-23 13:30:05 +0000

    init graphql server

---
+++ src/graphql/Category.ts
@@ -0,0 +1,38 @@
+import {
+    Column,
+    CreateDateColumn,
+    Entity,
+    PrimaryGeneratedColumn,
+    Unique,
+    UpdateDateColumn,
+    ManyToOne,
+    JoinColumn,
+    OneToMany,
+  } from "typeorm";
+import { Product } from "./Product";
+
+  @Entity()
+
+  @Unique(["code"])
+
+  export class Category{
+    @PrimaryGeneratedColumn()
+    readonly id!:string
+
+    @Column({type:"varchar"})
+    code!:string
+
+    @Column({type:"varchar"})
+    name!:string
+
+    @CreateDateColumn()
+    createdAt!:Date
+
+    @UpdateDateColumn()
+    updatedAt!:Date
+
+    @OneToMany((_type=>Product),(product:Product)=>product.id)
+    @JoinColumn({name:"productId"})
+    product!:Product
+  }
+  
