f32ed23cb5b51a3c2acd8c292a80e65568b0b6a6
Anddy Luciano <luciano.ratsimbazafindramanana@gmail.com>
2023-10-30 10:39:08 +0100

    modules for creating category

---
+++ src/graphql/modules/category/CreateCategoryInput.ts
@@ -0,0 +1,10 @@
+import { Field, InputType } from "type-graphql";
+import { Category } from "../../models/Category";
+
+@InputType()
+export class CreateCategoryInput implements Partial<Category> {
+    @Field()
+    public name!: string;
+    @Field()
+    public code!: string;
+}
