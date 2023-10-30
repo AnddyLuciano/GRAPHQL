e646bfdaf8b2905b300274e694320a07e57fd60e
Anddy Luciano <luciano.ratsimbazafindramanana@gmail.com>
2023-10-30 10:37:19 +0100

    updating product models

--- src/graphql/models/Product.ts
+++ src/graphql/models/Product.ts
@@ -6,31 +6,31 @@ import {
     Unique,
     UpdateDateColumn,
     ManyToOne,
-  } from "typeorm";
-  
-  import { Category } from "./Category";
+} from "typeorm";
+
+import { Category } from "./Category";
 import { Field, ObjectType } from "type-graphql";
-  
-  @ObjectType()
-  @Entity()
-  @Unique(["code"])
-  export class Product {
+
+@ObjectType()
+@Entity()
+@Unique(["code"])
+export class Product {
     @PrimaryGeneratedColumn()
     public readonly id!: number;
-  
+
     @Column({ type: "varchar" })
     public code!: string;
-  
+
     @Column({ type: "varchar" })
     public name!: string;
-  
+
     @CreateDateColumn()
     public createdAt!: Date;
-  
+
     @UpdateDateColumn()
     public updatedAt!: Date;
-  
+
     @Field((_type) => Category)
     @ManyToOne((_type) => Category, (category: Category) => category.products)
-    public category!: Category;
-  }
+    public category?: Category;
+}
